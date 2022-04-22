f626aece01352a2333c238af3fa41beebcf6aa4e
Adnan-Alfadhli <adnan2000611@gmail.com>
2022-04-22 23:10:49 +0300

    init

---
+++ Challenge-Plans/Challenge-Plans/Challenge_PlansApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_PlansApp.swift
+//  Challenge-Plans
+//
+//  Created by Adnan Alfadhli on 22/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_PlansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
